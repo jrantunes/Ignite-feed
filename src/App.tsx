ba54c3da26a4aa49f34b7c852079175e3faf649c
José neto <joseantunes2001@gmail.com>
2022-07-31 11:06:52 -0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Header } from "./components/Header";
+
 import "./global.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Hello World!!</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styles from "./header.module.css";
+
+import igniteLogo from "../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+  );
+}
